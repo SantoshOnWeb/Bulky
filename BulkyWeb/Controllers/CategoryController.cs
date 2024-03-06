dcb29a1776c248244aedbd02b83cfbb227061cce
Santosh <Lalit>
2024-03-06 15:59:36 +0530

    06/03/2024_Edit/Delete-category

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -33,9 +33,67 @@ namespace BulkyWeb.Controllers
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
+                TempData["Success"] = "Category Created Successfully";
                 return RedirectToAction("Index"/*,"Category"*/);   //Redirecting to index (action name). we skip controller name as we are in same controller
             }
             return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.FirstOrDefault(u=>u.Id==id);
+            //Category categoryFromDb1 = _db.Categories.Find(id);   // find works only on prim.key
+            if(categoryFromDb==null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if(ModelState.IsValid) 
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["Success"] = "Category Updated Successfully";
+                return RedirectToAction("Index"/*,"Category"*/);   //Redirecting to index (action name). we skip controller name as we are in same controller
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.FirstOrDefault(u=>u.Id==id);
+            //Category categoryFromDb1 = _db.Categories.Find(id);   // find works only on prim.key
+            if(categoryFromDb==null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePost(int? id)
+        {
+            Category? obj = _db.Categories.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["Success"] = "Category Deleted Successfully";
+            return RedirectToAction("Index"/*,"Category"*/);   //Redirecting to index (action name). we skip controller name as we are in same controller
         }
     }
 }
