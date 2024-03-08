378d5621854e12282183c6277ebca2b4b3de6e10
Santosh <Lalit>
2024-03-08 14:10:02 +0530

    08/03/2024_ Separated-data&models-to-diff-projects

--- Bulky.Models/Category.cs
+++ Bulky.Models/Category.cs
@@ -1,20 +1,20 @@
 ﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
-namespace BulkyWeb.Models
+namespace Bulky.Models
 {
     public class Category
     {
         [Key]
-        public  int Id { get; set; }
+        public int Id { get; set; }
 
         [Required]
         [MaxLength(25)]
         [DisplayName("Category Name")]
-        public  string? Name { get; set; }
+        public string? Name { get; set; }
 
         [DisplayName("Display Order")]
-        [Range(1,100,ErrorMessage ="Display Order must be between 1-100")]
-        public  int DisplayOrder { get; set; }
+        [Range(1, 100, ErrorMessage = "Display Order must be between 1-100")]
+        public int DisplayOrder { get; set; }
     }
 }
